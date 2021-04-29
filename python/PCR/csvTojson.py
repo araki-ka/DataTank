789eb53c5bc72fdeccfa96b8f3ce2a695598c33b
Kazuya Araki <lakeel.l0545@gmail.com>
2021-04-29 16:12:26 +0900

    Add: PCR Test data

--- python/PCR/csvTojson.py
+++ python/PCR/csvTojson.py
@@ -4,13 +4,25 @@ import io
 import pandas as pd
 import json
 
-CSVFILE = "https://www.mhlw.go.jp/content/pcr_positive_daily.csv"
-JSONFILE = "pcr_positive_daily.json"
+PCR_TEST_CSVFILE = "https://www.mhlw.go.jp/content/pcr_tested_daily.csv"
+COVID_POSITIVE_CSVFILE = "https://www.mhlw.go.jp/content/pcr_positive_daily.csv"
+JSONFILE = "covid_report.json"
 
-# get CSV data
-data = requests.get(CSVFILE).content
-df = pd.read_csv(io.BytesIO(data))
-df = df.rename(columns={"日付": "date", "PCR 検査陽性者数(単日)": "positives"})
+# get data
+## PCR Test
+pcr_data = requests.get(PCR_TEST_CSVFILE).content
+df_pcr = pd.read_csv(io.BytesIO(pcr_data))
+df_pcr = df_pcr.rename(columns={"日付": "date", "PCR 検査実施件数(単日)": "tests"})
+df_pcr = df_pcr.astype({"tests": object})
+
+## COVID Positive
+covid_data = requests.get(COVID_POSITIVE_CSVFILE).content
+df_covid = pd.read_csv(io.BytesIO(covid_data))
+df_covid = df_covid.rename(columns={"日付": "date", "PCR 検査陽性者数(単日)": "positives"})
+df_covid = df_covid.astype({"positives": object})
+
+## merge  data
+df = pd.merge(df_covid[["date", "positives"]], df_pcr[["date", "tests"]], how="left", on="date")
 
 # write to json file
 with open("../../data/PCR/" + JSONFILE, "w", encoding="utf_8") as jsonfile:
