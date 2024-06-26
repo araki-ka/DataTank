0bbe38718e198870cb7ad828b5f57d7c18f8137c
Kazuya Araki <lakeel.l0545@gmail.com>
2024-06-26 14:23:57 +0900

    Refactor: image_sampling

---
+++ src/image_sampling/__main__.py
@@ -0,0 +1,5 @@
+from . import main
+
+if __name__ == "__main__":
+  # main.image_to_csv("/Users/kazuya.araki/Development/Private/DataTank/images/YOASOBI.jpg")
+  main.image_to_csv("/Users/kazuya.araki/Development/Private/DataTank/images/Lenna.png")
